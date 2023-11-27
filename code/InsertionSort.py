d7bd1db92ab0128274b121e5054583c38b6ea177
Kennedy-Torres <krtorres.programador@hotmail.com>
2023-11-27 10:38:16 -0300

    arquivo 02_fontes com os codigos concatenados

--- code/InsertionSort.py
+++ code/InsertionSort.py
@@ -1,30 +1,31 @@
-class Medicamento: 
-    def __init__(self, nome, preco): 
-        self.nome = nome 
-        self.preco = preco 
+def ordena_medicamentos_usando_InsertionSort():
+    class Medicamento: 
+        def __init__(self, nome, preco): 
+            self.nome = nome 
+            self.preco = preco 
 
-def insertion_sort_farmacia(medicamentos): 
-    for i in range(1, len(medicamentos)): 
-        chave = medicamentos[i]  
-        j = i - 1 
+    def insertion_sort_farmacia(medicamentos): 
+        for i in range(1, len(medicamentos)): 
+            chave = medicamentos[i]  
+            j = i - 1 
 
-        while j >= 0 and chave.preco < medicamentos[j].preco: 
-            medicamentos[j + 1] = medicamentos[j] 
-            j -= 1 
+            while j >= 0 and chave.preco < medicamentos[j].preco: 
+                medicamentos[j + 1] = medicamentos[j] 
+                j -= 1 
 
-        medicamentos[j + 1] = chave  
-medicamento1 = Medicamento("Paracetamol", 15.50) 
-medicamento2 = Medicamento("Aspirina", 10.20)
-medicamento3 = Medicamento("Amoxicilina", 25.00)
-medicamento4 = Medicamento("Ibuprofeno", 12.75)
+            medicamentos[j + 1] = chave  
+    medicamento1 = Medicamento("Paracetamol", 15.50) 
+    medicamento2 = Medicamento("Aspirina", 10.20)
+    medicamento3 = Medicamento("Amoxicilina", 25.00)
+    medicamento4 = Medicamento("Ibuprofeno", 12.75)
 
-lista_medicamentos = [medicamento1, medicamento2, medicamento3, medicamento4] 
+    lista_medicamentos = [medicamento1, medicamento2, medicamento3, medicamento4] 
 
-insertion_sort_farmacia(lista_medicamentos) 
+    insertion_sort_farmacia(lista_medicamentos) 
 
-print("+=====================================+")
-print("|            Insertion Sort           |")
-print("+=====================================+")
-print("\nLista de medicamentos ordenada por preço:\n")
-for medicamento in lista_medicamentos:
-    print(f"{medicamento.nome}: R${medicamento.preco:.2f}") 
+    print("+=====================================+")
+    print("|            Insertion Sort           |")
+    print("+=====================================+")
+    print("\nLista de medicamentos ordenada por preço:\n")
+    for medicamento in lista_medicamentos:
+        print(f"{medicamento.nome}: R${medicamento.preco:.2f}") 
